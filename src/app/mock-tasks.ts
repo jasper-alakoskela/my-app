511d539db5fa0318e00d4941998d0099c6c569b8
Jasper Alakoskela <jasper.alakoskela@gmail.com>
2021-12-17 09:19:33 +0200

    angular-app

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm",
+        reminder: true,
+    },
+    {
+        id:2,
+        text: "School",
+        day: "February 12th at 8:30am",
+        reminder: false,
+    },
+    {
+        id:3,
+        text: "Chores",
+        day: "July 2nd at 3:00pm",
+        reminder: true,
+    },
+];
